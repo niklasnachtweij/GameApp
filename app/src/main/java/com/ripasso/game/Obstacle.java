346e359934975aabe572e2b3cbbf1f0129208ab0
niklasnachtweij <n.nachtweij@gmail.com>
2015-11-24 12:01:14 +0100

    Constraints for menu added Menu class updated.

--- app/src/main/java/com/ripasso/game/Obstacle.java
+++ app/src/main/java/com/ripasso/game/Obstacle.java
@@ -1,8 +1,18 @@
 package com.ripasso.game;
 
+import android.graphics.Canvas;
+import android.graphics.Rect;
+
 public class Obstacle extends GameObject {
 
+    @Override
+    public void onDraw(Canvas canvas) {
 
+    }
 
+    @Override
+    public Rect getBounds() {
+        return null;
+    }
 }
 
