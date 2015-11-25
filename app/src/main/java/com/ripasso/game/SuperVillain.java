02a513a1a4a3fec15de5156770691ebdac10d191
mathiasbernie <mathias.berneland@gmail.com>
2015-11-25 14:25:58 +0100

    Created SuperVillain and ability for game over

---
+++ app/src/main/java/com/ripasso/game/SuperVillain.java
@@ -0,0 +1,79 @@
+package com.ripasso.game;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+
+import java.util.Random;
+
+public class SuperVillain extends GameObject {
+    // direction = 0 up, 1 left, 2 down, 3 right,
+    // animation = 3 back, 1 left, 0 front, 2 right
+    int[] DIRECTION_TO_ANIMATION_MAP = {3, 1, 0, 2};
+    private static final int BMP_ROWS = 4;
+    private static final int BMP_COLUMNS = 3;
+    private int max_speed = 5;
+    private GameView gameView;
+    private Bitmap bmp;
+    private int xSpeed;
+    private int ySpeed;
+    private int currentFrame = 0;
+    private int width;
+    private int height;
+
+    public SuperVillain(GameView gameView, Bitmap bmp) {
+        this.width = bmp.getWidth() / BMP_COLUMNS;
+        this.height = bmp.getHeight() / BMP_ROWS;
+        this.gameView = gameView;
+        this.bmp = bmp;
+
+        Random rnd = new Random();
+        x = rnd.nextInt(gameView.getWidth() - width);
+        y = rnd.nextInt(gameView.getHeight() - 200 - height);
+        xSpeed = max_speed;
+        ySpeed = max_speed;
+    }
+
+    protected void update() {
+        if (x >= gameView.getWidth() - width - xSpeed || x + xSpeed <= 0) {
+            xSpeed = -xSpeed;
+        }
+        x = x + xSpeed;
+        if (y >= gameView.getHeight() - 200 - height - ySpeed || y + ySpeed <= 0) {
+            ySpeed = -ySpeed;
+        }
+        y = y + ySpeed;
+        currentFrame = ++currentFrame % BMP_COLUMNS;
+    }
+
+    public void onDraw(Canvas canvas) {
+        update();
+        int srcX = currentFrame * width;
+        int srcY = getAnimationRow() * height;
+        Rect src = new Rect(srcX, srcY, srcX + width, srcY + height);
+        Rect dst = new Rect((int) x, (int) y, (int) x + width, (int) y + height);
+
+        if (canvas != null)
+            canvas.drawBitmap(bmp, src, dst, null);
+    }
+
+    private int getAnimationRow() {
+        double dirDouble = (Math.atan2(xSpeed, ySpeed) / (Math.PI / 2) + 2);
+        int direction = (int) Math.round(dirDouble) % BMP_ROWS;
+        return DIRECTION_TO_ANIMATION_MAP[direction];
+    }
+
+    public Rect getBounds() {
+        return new Rect((int) x, (int) y, (int) x + this.width, (int) y + this.height);
+    }
+
+    public boolean isCollision(float x2, float y2) {
+        return x2 > x && x2 < x + width && y2 > y && y2 < y + height;
+    }
+
+    public void increaseSpeed() {
+
+        this.max_speed =+ 5;
+
+    }
+}
