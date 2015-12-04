24cd08d26c4351891bb01ce79fd2d3be97bdde0f
niklasnachtweij <n.nachtweij@gmail.com>
2015-12-04 18:42:31 +0100

    Backlog updated

--- app/src/main/java/com/ripasso/game/Backlog.java
+++ app/src/main/java/com/ripasso/game/Backlog.java
@@ -13,24 +13,27 @@ public class Backlog {
 
     3. Add Super Villian that can kill Hero *Must be different gfx wise than "usual" villain (Owner Mathias Berneland)
 
-    4. Add first-screen that explains how the game works
-
     5. Add graphical objects like health for hero on screen
 
             5.1 Hero should have health wich can be altered
             5.2 Villian -||-
 
-    6. Game should be able to complete or game over
+
+
+    FIXED // :
+
+    4. Add first-screen that explains how the game works //FIXED
+
+    6. Game should be able to complete or game over //FIXED
 
     7. When Hero and SuperVillain is colliding, make a new Activity with a new View start. Put
-        the highscore as an intent to the new activity.
+    the highscore as an intent to the new activity. // FIXED
+
 
 
     Bug fixes.
 
     "Yeah we are not super heros. Pretty damn close but we actually could have some minor bugs."
-
-
 
     1.  When onSurfaceDestroyed() in GameView.java is trying to stop or
         pause music in audioController.pauseBackgroundMusic(), LINE 63,
