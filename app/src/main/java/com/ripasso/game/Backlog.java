d93b735f1ef3f033f25eaa769990f31e5512c57f
niklasnachtweij <n.nachtweij@gmail.com>
2015-12-06 11:50:01 +0100

    Updated Backlog

--- app/src/main/java/com/ripasso/game/Backlog.java
+++ app/src/main/java/com/ripasso/game/Backlog.java
@@ -11,18 +11,18 @@ public class Backlog {
 
     2. Collision control between villains, hero and obstacles (Owner Niklas Nachtweij)
 
-    3. Add Super Villian that can kill Hero *Must be different gfx wise than "usual" villain (Owner Mathias Berneland)
-
-    5. Add graphical objects like health for hero on screen
-
-            5.1 Hero should have health wich can be altered
-            5.2 Villian -||-
-
 
 
     FIXED // :
 
     4. Add first-screen that explains how the game works //FIXED
+
+    3. Add Super Villian that can kill Hero *Must be different gfx wise than "usual" villain (Owner Mathias Berneland) // FIXED
+
+        5. Add graphical objects like health for hero on screen
+
+            5.1 Hero should have health wich can be altered //FIXED
+            5.2 Villian -||-    //FIXED
 
     6. Game should be able to complete or game over //FIXED
 
